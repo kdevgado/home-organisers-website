478565f96815dfc3631aa2d99a01dc7f1ab551ae
s3841649 <s3841649@student.rmit.edu.au>
2025-08-15 12:49:55 +1000

    fix deploy path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://github.com/kdevgado/home-organisers-website",
-  base: "/home-organisers-website/", 
+  site: "https://kdevgado.github.io",
+  base: "/home-organisers-website/",
   output: "static",
   trailingSlash: "always",
 });
