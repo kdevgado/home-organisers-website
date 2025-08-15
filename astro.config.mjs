42eaf13439793a8cf341ca9148d7db290b1ae66b
s3841649 <s3841649@student.rmit.edu.au>
2025-08-15 11:10:50 +1000

    Fix base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: "https://github.com/kdevgado/home-organisers-website",
+  base: "/home-organisers-website/", 
   output: "static",
+  trailingSlash: "always",
 });
