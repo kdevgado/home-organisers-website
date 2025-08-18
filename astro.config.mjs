0627c8472c8a030364267bb1721ed9aabd3041c4
s3841649 <s3841649@student.rmit.edu.au>
2025-08-18 22:01:45 +1000

    fix css to be global

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://kdevgado.github.io",
+  site: "https://homeorg.com.au",
   output: "static"
 });
