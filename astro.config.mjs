a42219ed2e602d8ef2592f59cfcf8ad548fddcad
s3841649 <s3841649@student.rmit.edu.au>
2025-08-18 21:29:05 +1000

    fix deployment assets for website

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: "https://kdevgado.github.io",
-  base: "/home-organisers-website/",
-  output: "static",
-  trailingSlash: "always",
+  output: "static"
 });
